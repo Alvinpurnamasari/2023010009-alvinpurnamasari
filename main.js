51564d2d6289940cf08a16ff767d8201f243f106
Alvinpurnamasari <162394212+Alvinpurnamasari@users.noreply.github.com>
2024-04-19 13:58:01 +0700

    commit pertama

--- main.js
+++ main.js
@@ -1,19 +1,2 @@
-document.title = 'alvin purnama'
-const btn1 = document.getElementById('btn1')
-const btn2 = document.querySelector('button')
-
-btn1.style.border = 'none'
-btn1.style.padding = '8px'
-btn1.style.fontSize = '24px'
-btn1.style.background = 'tomato'
-
-function gantiWarna() {
-btn1.style.background = 'pink'
-}
-
-function ubahText() {
-   btn1.textContent = 'hihi haha'
-}
-function oriText() {
-    btn1.textContent = 'klik saya 1'
-}
+const arraySaya = ['😊','🙌','👄']
+console.log(arraySaya)
