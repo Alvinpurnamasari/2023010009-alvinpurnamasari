440ccdf645f075b0a4ef03ceee1c11090d7621bb
Alvinpurnamasari <162394212+Alvinpurnamasari@users.noreply.github.com>
2024-05-08 19:57:42 +0700

    commit pertama

--- alal/dataType/string.js
+++ alal/dataType/string.js
@@ -1,2 +1,8 @@
 let greet = "Hello world";
 console.log(typeof(greet));
+
+const myname = "alvin";
+console.log(`Hai nama saya ${myname}`);
+
+const myadress = "jepara";
+console.log(`rumah saya ada di ${myadress}`);
