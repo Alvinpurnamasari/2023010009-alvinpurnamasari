e2af0111d684bf1c0c7d7ac81dfc5e8906dc8319
Alvinpurnamasari <162394212+Alvinpurnamasari@users.noreply.github.com>
2024-05-08 19:50:35 +0700

    commit pertama

---
+++ alal/dataType/BigInt.js
@@ -0,0 +1,11 @@
+const bigNumber = 1234567890n;
+const myInt = 1234567890;
+
+console.log(bigNumber);
+console.log(myInt);
+
+console.log(100n + 70n)
+console.log(100n - 70n)
+console.log(100n * 70n)
+console.log(100n / 70n)
+console.log(100n % 70n)
